1024326ec909dc29f9973a0a48ec80d7f93b92bf
Matheus <m.dias@univille.br>
2023-05-02 13:59:37 -0300

    SeparandoFronteBack

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const {VueLoaderPlugion} = require('vue-loader');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-module.exports = {
-    entry:'./src/main.js'
-};
