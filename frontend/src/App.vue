8610a0d6f54912ff8177b7b77ea4ff76a149e14a
matheusfrd <felip3.dias@gmail.com>
2023-05-03 00:21:59 -0300

    teste2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        auiha
+        aiuhauihauha
       </nav>
     </div>
   </header>
