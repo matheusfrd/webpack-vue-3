fb13a313c0229344d85a4a144f293f137f4e3249
matheusfrd <felip3.dias@gmail.com>
2023-05-19 00:21:23 -0300

    front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
+// import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp({
+  template:`
+    <header-app></header-app>
+    auihauiha
+    <footer-app></footer-app>
+    `
+})
+
+app.component('header-app',{
+  template:'<h2>Header</h2>'
+})
+
+app.component('footer-app',{
+  template:'<h2>footer</h2>'
+})
 
 app.use(router)
 
